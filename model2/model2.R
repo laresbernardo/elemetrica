2e22581216e56815e9e0ec735813f9fd178315b4
Bernardo Lares <laresbernardo@gmail.com>
2020-10-05 20:15:15 -0300

    std

--- model2/model2.R
+++ model2/model2.R
@@ -1,21 +1,35 @@
 source("funs.R")
 
 level <- 4
+type <- 2
+min_per_cat <- 10
+dropnumwords <- FALSE
+minchar <- 1
+w2v.epochs <- 16
+w2v.window <- 6
+w2v.vecs <- 150
+train_p <- 1
 
 # Read the data
-cats <- read_data(type = 2)
+cats <- read_data(type = type)
 # Prepare dataset
-df <- prepare_data(cats, level = level, minimum = 10, type = 2)
+df <- prepare_data(cats, level = level, 
+                   minimum = min_per_cat, 
+                   type = type)
+# Print summary and most frequents
 summary_data(df, cats)
 
 # Tokenize: Break labels into sequence of words
-words <- h2o_tokenize(df$product, dropnumwords = FALSE, minchar = 1, stop_words = STOP_WORDS)
+words <- h2o_tokenize(df$product, 
+                      dropnumwords = dropnumwords, 
+                      minchar = minchar, 
+                      stop_words = STOP_WORDS)
 
 # Create word2vector model
 w2v.model <- h2o_word2vec(words, 
-                          epochs = 16, 
-                          window_size = 6, 
-                          vec_size = 150) 
+                          epochs = w2v.epochs, 
+                          window_size = w2v.window, 
+                          vec_size = w2v.vecs) 
 
 # Prepare training & validation data (keep only labels made of known words)
 data <- df %>% bind_cols(as.data.frame(w2v.model$vectors)) %>% filter(!is.na(C1))
@@ -26,7 +40,7 @@ modelx <- h2o_automl(
   y = "category", 
   ignore = c("product", "chain"),
   max_models = 1,
-  split = 1,
+  split = train_p,
   thresh = 500, 
   start_clean = FALSE,
   include_algos = "DRF",
@@ -55,6 +69,7 @@ model$scores_test %>%
 # Manual testings
 sample_n(df, 10) %>% clean_label(c("product","category"))
 h2o_word2vec.predict("tubo pvc", w2v.model$model, model$model, clean = TRUE)
+h2o_word2vec.predict("tubo cpvc", w2v.model$model, model$model, clean = TRUE)
 h2o_word2vec.predict("extension electrica", w2v.model$model, model$model, clean = TRUE)
 h2o_word2vec.predict("botas de seguridad", w2v.model$model, model$model, clean = TRUE)
 h2o_word2vec.predict("martillo", w2v.model$model, model$model, clean = TRUE)
